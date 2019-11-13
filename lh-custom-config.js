e412a6e15bb7c4ab12e3a551be5a82dfcd43e7cc
lvolfman <57180052+lvolfman@users.noreply.github.com>
2019-11-13 11:43:11 +0300

    Update lh-custom-config.js

--- lh-custom-config.js
+++ lh-custom-config.js
@@ -11,7 +11,7 @@ const config = {
   settings: {
     maxWaitForFcp: 15 * 1000,
     maxWaitForLoad: 35 * 1000,
-    emulatedFormFactor: 'desktop',
+    emulatedFormFactor: 'mobile',
     //throttling: {
       // Using a "broadband" connection type
       // Corresponds to "Dense 4G 25th percentile" in https://docs.google.com/document/d/1Ft1Bnq9-t4jK5egLSOc28IL4TvR-Tt0se_1faTA4KTY/edit#heading=h.bb7nfy2x9e5v
