ea367d5114fe90fe374bea3ad68de327f670ae8c
lvolfman <57180052+lvolfman@users.noreply.github.com>
2019-11-13 11:42:56 +0300

    Update lighthouse_exporter.js

--- lighthouse_exporter.js
+++ lighthouse_exporter.js
@@ -6,26 +6,11 @@ const http = require('http');
 const url = require('url');
 const puppeteer = require('puppeteer');
 const lighthouse = require('lighthouse');
-const chromeLauncher = require('chrome-launcher');
 const minimist = require('minimist');
 var Mutex = require('async-mutex').Mutex;
 const config = require('./lh-custom-config.js');
-const opts = {  chromeFlags: ['--show-paint-rects']};
 
 var argv = minimist(process.argv.slice(2));
-
-function launchChromeAndRunLighthouse(url, opts, config = null) {
-  return chromeLauncher.launch({chromeFlags: opts.chromeFlags}).then(chrome => {
-    opts.port = chrome.port;
-    return lighthouse(url, opts, config).then(results => {
-      // use results.lhr for the JS-consumeable output
-      // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
-      // use results.report for the HTML/JSON/CSV output as a string
-      // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-      return chrome.kill().then(() => results.lhr)
-    });
-  });
-}
 
 var port = 9593;
 
@@ -45,27 +30,24 @@ http.createServer(async (req, res) => {
         var data = [];
 
         try{
-            //const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+            const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
             
             data.push('# HELP LH_exporter fork version 0.2.9');
             data.push('# HELP lighthouse_exporter_info Exporter Info');
             data.push('# TYPE lighthouse_exporter_info gauge');
-            // data.push(`lighthouse_exporter_info{version="0.2.9",chrome_version="${await browser.version()}",node_version="${process.version}"} 1`);
+            data.push(`lighthouse_exporter_info{version="0.2.9",chrome_version="${await browser.version()}",node_version="${process.version}"} 1`);
 
-            launchChromeAndRunLighthouse(target, opts, config)
-            /**
             await lighthouse(target, 
                              {port: url.parse(browser.wsEndpoint()).port,output: 'json'}, 
                              config)
-             */
                 .then(results => {
                     data.push('# HELP lighthouse_score The Score per Category');
                     data.push('# TYPE lighthouse_score gauge');
 
                     for(var category in results.lhr.categories){
                         var item = results.lhr.categories[category];
-                        data.push(results);
-                        // data.push(`lighthouse_score{category="${category}"} ${item.score * 100}`);
+
+                        data.push(`lighthouse_score{category="${category}"} ${item.score * 100}`);
                     }
 
                     var audits = results.lhr.audits;
@@ -84,7 +66,7 @@ http.createServer(async (req, res) => {
                     console.error("Lighthouse", Date(), error);
                 });
 
-            //await browser.close();
+            await browser.close();
         } catch(error) {
             console.error("Generic", Date(), error);
         }
